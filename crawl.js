05e996c98d29fe15d7b336380f2e7e130cc916ed
Ilja Koreaus <iljakoskin@Ilja-Kannettava-tietokone.local>
2026-09-21 11:15:08 +0300

    Crawling Multiple Pages Recursively

--- crawl.js
+++ crawl.js
@@ -1,6 +1,27 @@
 const {JSDOM} = require('jsdom');
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+
+    const baseURLObj = new URL(baseURL);
+    const currnetURLObj = new URL(currentURL);
+
+    // Checking If We're Still On
+    // The Same Page
+    if (baseURLObj.hostname !== currnetURLObj.hostname) {
+        return pages;
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL);
+
+    // Counting The Amount Of The Page
+    // Has Occurred
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++;
+        return pages;
+    }
+
+    pages[normalizedCurrentURL] = 1;
+
     console.log(`Actively Crawling ${currentURL}`);
 
     try {
@@ -9,21 +30,29 @@ async function crawlPage(currentURL) {
         // Checking For Errors
         if (resp.status > 399) {
             console.log(`Error In Fetch With Status Code: ${resp.status} on page: ${currentURL}`);
-            return;
+            return pages;
         }
 
         const contentType = resp.headers.get("content-type");
 
         if (!contentType.includes("text/html")) {
             console.log(`Non HTML Response, Content Type: ${contentType} on page: ${currentURL}`);
-            return;
+            return pages;
         }
 
-        console.log(await resp.text());
+        const htmlBody = await resp.text();
+
+        nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages);
+        }
 
     } catch (err) {
         console.log(`Error In Fetch: ${err.message}, on page ${currentURL}`);
     }
+
+    return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {crawlPage} = require('./crawl.js');
 
-function main() {
+async function main() {
 
     // Check Command Line Arguments & Throw
     // Error If Necessary
@@ -20,7 +20,11 @@ function main() {
     const baseURL = process.argv[2];
 
     console.log(`Starting Crawl Of ${baseURL}`);
-    crawlPage(baseURL);
+    const pages = await crawlPage(baseURL, baseURL, {});
+
+    for (const page of Object.entries(pages)) {
+        console.log(page);
+    }
 };
 
 main();
